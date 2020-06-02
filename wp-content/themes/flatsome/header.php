e6b2cae55b8dffbbd07cc55883d930077a92785e
mkhomedecor <mkhomedecor@users.noreply.github.com>
2020-06-02 13:20:10 +0700

    up

---
+++ wp-content/themes/flatsome/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if IE 9 ]> <html <?php language_attributes(); ?> class="ie9 <?php flatsome_html_classes(); ?>"> <![endif]-->
+<!--[if IE 8 ]> <html <?php language_attributes(); ?> class="ie8 <?php flatsome_html_classes(); ?>"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="<?php flatsome_html_classes(); ?>"> <!--<![endif]-->
+<head>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MXSQDLB');</script>
+<!-- End Google Tag Manager -->
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168038831-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-168038831-1');
+</script>
+
+	<meta name="google-site-verification" content="BoXIRUqAKkr-lIYoIflCb6LQPaJPNRE0IsQ5RF8-rPM" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php do_action( 'flatsome_after_body_open' ); ?>
+<?php wp_body_open(); ?>
+
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'flatsome' ); ?></a>
+
+<div id="wrapper">
+
+	<?php do_action( 'flatsome_before_header' ); ?>
+
+	<header id="header" class="header <?php flatsome_header_classes(); ?>">
+		<div class="header-wrapper">
+			<?php get_template_part( 'template-parts/header/header', 'wrapper' ); ?>
+		</div>
+	</header>
+
+	<?php do_action( 'flatsome_after_header' ); ?>
+
+	<main id="main" class="<?php flatsome_main_classes(); ?>">
